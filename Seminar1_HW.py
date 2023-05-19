42f60c2876f365376f841f27d32d0549c1819e8b
AlekseiBelorukov <slimmail2011@gmail.com>
2023-05-19 12:52:49 +0300

    HW_1 Добавил и решил задачу №2

---
+++ Seminar1_HW.py
@@ -0,0 +1,17 @@
+# Задача 2: Найдите сумму цифр трехзначного числа. 
+# 123 -> 6 (1 + 2 + 3)
+# 100 -> 1 (1 + 0 + 0)
+
+number = int(input("Введите число: "))
+# number = 234
+finish_num = 0
+while number > 0 :
+    temp = number % 10
+    number //= 10
+    finish_num = finish_num + temp
+
+print(finish_num)
+
+
+
+
